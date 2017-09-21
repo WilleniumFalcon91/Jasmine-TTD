417716d3f4721bfbdc7e2509d4c3d5325bb6ff32
William Elliott <wcelliott1991@gmail.com>
2017-09-21 10:05:47 -0400

    initializes first passing test

---
+++ spec/fizzbuzzSpec.js
@@ -0,0 +1,5 @@
+describe("FizzBuz", function() {
+    it("should return 1 for int 1", function() {
+        expect(fizzBuzzTranslator(1)).toBe("1")
+    })
+})
---
+++ src/fizzbuzz.js
@@ -0,0 +1,3 @@
+function fizzBuzzTranslator() {
+    return "1";
+}
